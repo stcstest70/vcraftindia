3d87ca06c4223fff2fde499185cba45008ebb57c
JayPrakash <stcstest70@gmail.com>
2023-08-04 14:13:10 +0530

    new changes

--- src/components/login/ResetPassword.js
+++ src/components/login/ResetPassword.js
@@ -3,12 +3,14 @@ import React from 'react'
 import { useEffect, useState, Fragment } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom'
+import { Spinner } from 'react-bootstrap';
 
 const ResetPassword = () => {
 
     const [validUrl, setValidUrl] = useState(true);
 	const param = useParams();
     const id = param.id;
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
 		const verifyEmailUrl = async () => {
@@ -35,9 +37,15 @@ const ResetPassword = () => {
     const [password, setPassword] = useState('');
     const [cpassword, setCpassword] = useState('');
     const resetPassword = async (e)=>{
+        setLoading(true);
         e.preventDefault();
-        if(password!== cpassword){
+        if(!password || !cpassword){
+            window.alert('Please enter all fields');
+            setLoading(false);
+        }
+        else if(password!== cpassword){
             window.alert('Password and Confirm password do not match');
+            setLoading(false);
         }
         else{
             try {
@@ -53,13 +61,16 @@ const ResetPassword = () => {
                 });
                 if(res.status === 201){
                     window.alert('Password Updated successfully');
+                    setLoading(false);
                     navigate('/login');
                 }else if(res.status === 400){
                     window.alert('Please enter all fields');
+                    setLoading(false);
                 }
             } catch (error) {
                 console.log(error);
                 window.alert('Internal server error');
+                setLoading(false);
             }
         }
     }
@@ -71,14 +82,15 @@ const ResetPassword = () => {
         <form className="loginPage" method="POST">
           <h2> Reset Password</h2> <br /> <br />
           <div className='labF'> <label for="email"><i className="fa-solid fa-lock" />
-          </label> <input type="password" name="email" id="email" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label> <input type="password" name="email" id="email" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required/>
           </div> <br />
           <div className='labF'> <label for="password"><i className="fa-solid fa-lock" />
-          </label> <input type="password" name="userpassword" id="password" placeholder='Confirm Password' value={cpassword} onChange={(e) => setCpassword(e.target.value)} />
+          </label> <input type="password" name="userpassword" id="password" placeholder='Confirm Password' value={cpassword} onChange={(e) => setCpassword(e.target.value)} required/>
           </div> <br />
   
           <div className="signIn">
-            <button type="submit" name="signIn" onClick={resetPassword}>Reset Password</button> 
+            {loading ? <Spinner animation="border" /> : <button type="submit" name="signIn" onClick={resetPassword}>Reset Password</button>  }
+            
           </div>
         </form>
       </div>
